b863c7cc68c1ebd44fef5a9ff10e19ba8b6bf601
GaiKT <124340777+GaiKT@users.noreply.github.com>
2024-06-12 18:15:53 +0700

    feat : get user , update user in auth router

--- apps/auth.js
+++ apps/auth.js
@@ -72,11 +72,12 @@ authRouter.post("/login", async (req, res) => {
       id: user.rows[0].user_id,
       firstName: user.rows[0].firstname,
       lastName: user.rows[0].lastname,
-      UserName: user.rows[0].username
+      UserName: user.rows[0].username,
+      level: user.rows[0].level
     },
       process.env.SECRET_KEY,
     {
-      expiresIn: "2000000",
+      expiresIn: "20000000",
     }
     );
 
@@ -85,5 +86,67 @@ authRouter.post("/login", async (req, res) => {
       token
     });
 });
+
+authRouter.put("/update", async (req, res) => {
+  const user = {
+    ...req.body,
+    updated_at: new Date(),
+  };
+
+  if(!user){
+    return res.status(401).json({
+      message: "update faild this not request data",
+    });
+  }
+
+  try {
+    await pool.query(
+      `insert into users ( username,firstname, lastname, level, updated_at)
+      values ($1, $2, $3, $4, $5 , $6 ,$7)`,
+      [
+        user.username,
+        user.firstname,
+        user.lastname,
+        user.level,
+        user.updated_at,
+      ]
+    );
+  
+    return res.status(200).json({
+      message: "User has been created successfully",
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message: error,
+    });
+  }
+
+});
+
+authRouter.get("/users" , async (req , res)=> {
+  const keyword = req.query.keyword
+  let users = []
+
+  try {
+    if(keyword){
+      users = await pool.query(`SELECT * 
+      FROM users 
+      WHERE username ILIKE $1 OR firstname ILIKE $1 OR lastname ILIKE $1
+      ORDER BY level DESC
+      ` ,[`%${keyword}%`])
+    }else{
+      users = await pool.query(`SELECT * FROM users ORDER BY level DESC`)
+    }
+    return res.status(200).json({
+      data : users.rows
+    });
+  } catch (error) {
+    return res.status(404).json({
+      message : error
+    });
+  }
+
+})
+
 
 export default authRouter;
